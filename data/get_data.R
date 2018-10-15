3f25d6d488b6e4d5c35cefc8f0c17d93b0e3ff64
fawda123 <mbafs2012@gmail.com>
2018-10-15 14:53:05 -0700

    working on wrangleplot up to joins, simplified example datasets

--- data/get_data.R
+++ data/get_data.R
@@ -4,13 +4,11 @@ library(tidyverse)
 
 ascidat <- read.csv('https://raw.githubusercontent.com/fawda123/ASCIfigs/master/raw/asci.scores.csv', stringsAsFactors = F) %>% 
   select(X, Type.hybrid.1, MMI.hybrid) %>% 
-  mutate(
-    Replicate = gsub('.*_([0-9]+)$', '\\1', X), 
-    X = gsub('_[0-9]+$', '', X),
-    SampleDate = gsub('.*_(.*)$', '\\1', X),
-    StationID = gsub('(^.*)_.*$', '\\1', X)
-  ) %>% 
-  select(-X)
+  rename(
+    id = X, 
+    site_type = Type.hybrid.1,
+    ASCI = MMI.hybrid
+  )
 cscidat <- read.csv('https://raw.githubusercontent.com/fawda123/ASCIfigs/master/raw/csci.scores.csv', stringsAsFactors = F) %>% 
   mutate(
     Replicate = gsub('.*_([0-9]+)$', '\\1', SampleID_old), 
@@ -19,16 +17,6 @@ cscidat <- read.csv('https://raw.githubusercontent.com/fawda123/ASCIfigs/master/
     StationID = gsub('(^.*)_.*$', '\\1', SampleID_old)
   ) %>% 
   select(StationID, SampleDate, Replicate, CSCI, COMID)
-
-locs <- read.csv('https://raw.githubusercontent.com/fawda123/ASCIfigs/master/raw/csci.scores.csv', stringsAsFactors = F) %>% 
-  mutate(
-    Replicate = gsub('.*_([0-9]+)$', '\\1', SampleID_old), 
-    SampleID_old = gsub('_[0-9]+$', '', SampleID_old),
-    SampleDate = gsub('.*_(.*)$', '\\1', SampleID_old),
-    StationID = gsub('(^.*)_.*$', '\\1', SampleID_old)
-  ) %>% 
-  select(StationID, New_Lat, New_Long) %>% 
-  unique
 
 write.csv(ascidat, file = 'data/ascidat.csv', row.names = F, quote = F)
 write.csv(cscidat, file = 'data/cscidat.csv', row.names = F, quote = F)
